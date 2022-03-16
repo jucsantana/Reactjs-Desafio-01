7426d67b54113bed5610948db9a31c77ba9dc1f3
Julio Santana <jucsantana@gmail.com>
2022-03-16 16:41:20 -0300

    Conclusão do desafio 01

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return; 
+    const task ={
+        id:Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+    setTasks( t => [...t, task] );
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(
+              tasks.map(t => {
+                        if(t.id==id){
+                          t.isComplete = !t.isComplete
+                        }
+                        return t;
+                    }) 
+            )
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks:Task[] = tasks.filter(t => t.id != id );
+    setTasks(newTasks);
   }
 
   return (
